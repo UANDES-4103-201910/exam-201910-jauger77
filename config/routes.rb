90c0dc817435d6aca078a8aa8d62f7446e5d5d47
Jaime <jiauger@miuandes.cl>
2019-06-18 12:14:52 -0400

    fixing mistakes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  get 'home/index'
+  root to: "home#index"
+
   resources :order_products
   resources :orders
   resources :addresses
   resources :products
-  devise_for :users
-  get 'home/index'
-  root to: "home#index"
+  
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
