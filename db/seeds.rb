00c7a80dd7db8d4285004649fd40fdfb3934d14d
Jaime <jiauger@miuandes.cl>
2019-06-18 12:35:42 -0400

    dont be on time to finish seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+
+
+#address1 = Address.create(phone: 123456789 , addressline1: 'Rodrigo de Triana 4225' ,addressline2: 'depto 1001' city: 'Santiago',country:'Chile',zipcode: 755455 ,user_id: user1)
+#user1 = User.create(email: 'jiauger@miuandes.cl', encrypted_password: 'jaime1234', firstname: 'Jaime', lastname: 'Auger')
+ 
