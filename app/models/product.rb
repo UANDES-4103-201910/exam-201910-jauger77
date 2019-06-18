01cebbc2fccbe488a09f59b65b24b2f1416bc3dd
Jaime <jiauger@miuandes.cl>
2019-06-18 11:37:34 -0400

    last validates

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+validates :shortdescription, length: {in: 5..40}
+validates :longdescription, length: {in: 15..500}
+validates :brand, :model, :price, :shortdecription, :longdescription, presence: true
 end
